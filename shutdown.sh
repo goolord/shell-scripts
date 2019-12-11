fdf3389cb63232d4e54e73b81904512ce7cd1e0e
Zachary Churchill <zacharyachurchill@gmail.com>
2019-12-10 23:58:10 -0500

    no wrapping in question dialog

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ACTION=`zenity --width=90 --height=250 --list --radiolist --text="Select logout action" --title="Logout"  \
+ACTION=`zenity --width=250 --height=300 --list --radiolist --text="Select logout action" --title="Logout"  \
   --column "Choice" --column "Action" \
   TRUE Shutdown \
   FALSE Reboot \
@@ -12,16 +12,16 @@ ACTION=`zenity --width=90 --height=250 --list --radiolist --text="Select logout 
 if [ -n "${ACTION}" ];then
   case $ACTION in
   Shutdown)
-    zenity --question --text "Are you sure you want to halt?" && systemctl poweroff
+    zenity --question --no-wrap --text "Are you sure you want to halt?" && systemctl poweroff
     ;;
   Reboot)
-    zenity --question --text "Are you sure you want to reboot?" && systemctl reboot
+    zenity --question --no-wrap --text "Are you sure you want to reboot?" && systemctl reboot
     ;;
   Suspend)
     lock && systemctl suspend
     ;;
   'Log Out')
-    i3-msg exit
+    xfce4-session-logout --logout -f
     ;;
   'Lock Screen')
     lock
