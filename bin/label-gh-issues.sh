93263a98220721faffe854eb12eb811cbe7eddc0
Zachary Churchill <zacharyachurchill@gmail.com>
2023-02-14 14:43:50 -0500

    preview window size

--- bin/label-gh-issues.sh
+++ bin/label-gh-issues.sh
@@ -5,7 +5,7 @@ LABELS=$(echo $NONE; gh label list --json name | jq '.[] | .name' --raw-output)
 for i in $(gh issue list --json number,labels | jq '.[] | select(.labels==[]) | .number')
 do
   PREVIEW="gh issue view $i"
-  LABEL=$(echo $LABELS | fzf --multi --preview $PREVIEW)
+  LABEL=$(echo $LABELS | fzf --multi --preview $PREVIEW --preview-window='right:70%')
   if [ $LABEL = $NONE ];
   then
     echo "skipping $i"
